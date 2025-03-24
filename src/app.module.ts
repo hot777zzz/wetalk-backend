86201c796287dbbc1653e4504712c15a68c01e37
Hot7 <912275990@qq.com>
2025-03-24 16:46:12 +0800

    添加mongoose依赖，module引入数据库连接

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  // 填写本地的数据库路径
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/mongodb')],
   controllers: [AppController],
   providers: [AppService],
 })
